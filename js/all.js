5390d316161d15958a38d68370594681b4d5e87f
120061203 <ccssll120061203@gmail.com>
2021-06-08 01:57:55 +0800

    Feat:swiper banner ok

--- js/all.js
+++ js/all.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.dropDown').click(function(event){
         event.preventDefault();
-        // $('.dropDownOpen').slideToggle();
+        $('.dropDownOpen').slideToggle();
     });
 });
